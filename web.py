2b40d16d25f7b9c6b23b10249325352ff7872e63
Lynden Volpe <lynden.volpe@gmail.com>
2016-10-05 16:46:05 -0400

    Created an about page

--- web.py
+++ web.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 #we've created the app, but we haven't run it yet. 
 #8 lines of code has created an app!
 #adding debut = true allows you to change the code without 
