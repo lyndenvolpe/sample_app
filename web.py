96c27203429e5c79b311b75a422febfcce6da5c3
Lynden Volpe <Lynden Volpe>
2016-10-05 15:31:30 -0400

    First commit

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#creating the home page - first visit website page
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+#we've created the app, but we haven't run it yet. 
+#8 lines of code has created an app!
+#adding debut = true allows you to change the code without 
+#having to rerunning it every time.
+app.run(debug=True)
